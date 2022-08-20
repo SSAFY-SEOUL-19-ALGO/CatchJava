5ff05bf9abf23e32fa2a162c95bbc5d5c454eb02
SadoRuin <dope2514@gmail.com>
2022-08-21 03:08:08 +0900

    [2022-08-21 박윤환] P43162_네트워크 BFS 방식 풀이

--- src/week3_박윤환/P43162_네트워크.java
+++ src/week3_박윤환/P43162_네트워크.java
@@ -1,31 +1,31 @@
 package week3_박윤환;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class P43162_네트워크 {
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        int[] parent = new int[n];  // 각 노드의 부모노드를 기록할 배열
+        Queue<Integer> q = new ArrayDeque<>();  // 방문할 컴퓨터를 기록할 큐
+        boolean[] visited = new boolean[n]; // 방문 여부를 기록할 boolean 배열
 
         for(int i=0; i<n; i++) {
-            parent[i] = i;  // 초기 부모노드는 자기자신으로 초기화
-        }
+            if(visited[i]) continue;    // 방문한적 있다면 다음 컴퓨터로
 
-        for(int i=1; i<n; i++) {
-            for (int j=0; j<i; j++) {    // 무향그래프 이므로 절반만 탐색
-                if (computers[i][j] == 1) {  // 두 컴퓨터가 연결되어 있으면
-                    int tmp = parent[j];    // 값이 for문 반복 중에 바뀔수 있으므로 임시변수에 저장
-                    for (int k=0; k<n; k++) {
-                        if (parent[k] == tmp) {    // j와 부모가 같은 모든 노드를
-                            parent[k] = parent[i];  // i의 부모로 변경
-                        }
+            q.offer(i); // 현재 컴퓨터부터 탐색
+
+            while(!q.isEmpty()) {   // 방문할 컴퓨터가 없을때까지
+                int cur = q.poll(); // 현재 컴퓨터
+
+                for (int j = 0; j < n; j++) {
+                    if (!visited[j] && computers[cur][j] == 1) {    // 방문한적 없거나 연결된 컴퓨터라면
+                        visited[j] = true;  // 방문 처리를 하고
+                        q.offer(j); // 방문할 컴퓨터로 큐에 추가
                     }
                 }
             }
-        }
 
-        for(int i=0; i<n; i++) {
-            if(parent[i] == i) {    // 부모가 자기 자신인 노드 개수 = 집합 개수
-                answer++;
-            }
+            answer++;   // 연결된 모든 컴퓨터의 방문이 끝나면 하나의 네트워크 개수 증가
         }
 
         return answer;
