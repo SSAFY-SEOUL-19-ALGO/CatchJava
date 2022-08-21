406c39005ded2c2dc68dc8490930acefc32eee39
kunwoo park <scv74502@gmail.com>
2022-08-22 03:41:35 +0900

    week 3

---
+++ src/week3_박건우/P43162_네트워크.java
@@ -0,0 +1,35 @@
+package week3_박건우;
+
+public class P43162_네트워크 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
+
+class Solution_P43162 {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        boolean[] check = new boolean[n];
+        
+        for(int i = 0; i < n; i++) {
+        	if(check[i] == false) {
+        		dfs(computers, i, check);
+        		answer++;
+        	}
+        }
+        return answer;
+    }
+    
+    boolean[] dfs(int[][] computers, int i, boolean[] check) {
+    	check[i] = true;
+    	
+    	for(int j = 0; j < computers.length; j++) {
+    		if(i != j && computers[i][j] == 1 && check[j] == false) {
+    			check = dfs(computers, j, check);
+    		}
+    	}
+    	return check;
+    }
+}
