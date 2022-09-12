318fa7870608b3ed58586b1b6af2cebc6fd525e7
김보라 <bora2292@naver.com>
2022-09-13 08:32:23 +0900

    [2022-09-13 김보라] week6 문제풀이 완료

---
+++ src/week6/week6_김보라/BJ20115_에너지드링크.java
@@ -0,0 +1,33 @@
+package week6_김보라;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class BJ20115_에너지드링크 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		Integer arr[] = new Integer[N];
+		String s[] = br.readLine().split(" ");
+		for(int i = 0; i<N; i++) {
+			arr[i]=Integer.parseInt(s[i]);
+		}
+		
+		Arrays.sort(arr,Comparator.reverseOrder());
+		double answer=0.0;
+		
+		answer+=arr[0];
+		for(int i = 1; i<N; i++) {
+			answer+=(double)arr[i]/2;
+		}
+		
+		System.out.println(answer);
+		
+	}
+
+}
